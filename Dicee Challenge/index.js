9948a2cd0ee7f32dd5fb4e7f0745ea1d4948718b
Shubbhh18 <your.email@example.com>
2025-03-26 15:56:49 +0530

    Fixed JS loading issue

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function() {
   var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
   var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
   var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
@@ -9,12 +9,12 @@ window.onload = function () {
   var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
   document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-  // If player 1 wins
+  //If player 1 wins
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+      document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
   } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+      document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
   } else {
-    document.querySelector("h1").innerHTML = "Draw!";
+      document.querySelector("h1").innerHTML = "Draw!";
   }
 };
